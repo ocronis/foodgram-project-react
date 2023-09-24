5c05ff88fcdfdacb388bbcd95629074ed2b1c31e
ocronis <sorokidzes@yandex.ru>
2023-09-24 20:09:36 +0300

    sixth commit

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -26,7 +26,6 @@ from recipes.models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
 
 User = get_user_model()
 
-
 class RecipesViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     filter_backends = [DjangoFilterBackend]
@@ -43,20 +42,18 @@ class RecipesViewSet(viewsets.ModelViewSet):
         queryset = Recipe.objects.all()
         author = self.request.user
         if self.request.GET.get('is_favorited'):
-            favorite_recipes_ids = Favorite.objects
-            	.filter(user=author).values('recipe_id')
+            favorite_recipes_ids = Favorite.objects.filter(user=author).values('recipe_id')
             return queryset.filter(pk__in=favorite_recipes_ids)
 
         if self.request.GET.get('is_in_shopping_cart'):
-            cart_recipes_ids = ShoppingCart.objects
-            	.filter(user=author).values('recipe_id')
+            cart_recipes_ids = ShoppingCart.objects.filter(user=author).values('recipe_id')
             return queryset.filter(pk__in=cart_recipes_ids)
         return queryset
 
     def add_in_list(self, model, user, pk):
         if model.objects.filter(user=user, recipe__id=pk).exists():
             response_data = {'errors': f'Рецепт уже добавлен в {model.__name__}'}
-	    return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
         recipe = get_object_or_404(Recipe, pk=pk)
         model.objects.create(user=user, recipe=recipe)
         serializer = RecipeListSerializer(recipe)
@@ -67,9 +64,8 @@ class RecipesViewSet(viewsets.ModelViewSet):
         if obj.exists():
             obj.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-	error_message = f'Рецепт не добавлен в {model.__name__}'
-	return Response({'errors': error_message}, status=status.HTTP_400_BAD_REQUEST)
-
+        error_message = f'Рецепт не добавлен в {model.__name__}'
+        return Response({'errors': error_message}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(methods=['post', 'delete'], detail=True, permission_classes=[IsAuthenticated])
     def favorite(self, request, pk=None):
@@ -125,10 +121,10 @@ class FollowUserView(APIView):
     def post(self, request, id):
         author = get_object_or_404(User, id=id)
         if request.user.follower.filter(author=author).exists():
-		error_message = "Вы уже подписаны на автора"
-		return Response({"errors": error_message}, status=status.HTTP_400_BAD_REQUEST)
-	follow_instance = request.user.follower.create(author=author)
-	serializer = FollowSerializer(follow_instance, context={"request": request})
+            error_message = "Вы уже подписаны на автора"
+            return Response({"errors": error_message}, status=status.HTTP_400_BAD_REQUEST)
+        follow_instance = request.user.follower.create(author=author)
+        serializer = FollowSerializer(follow_instance, context={"request": request})
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def delete(self, request, id):
@@ -136,9 +132,8 @@ class FollowUserView(APIView):
         if request.user.follower.filter(author=author).exists():
             request.user.follower.filter(author=author).delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-	error_message = "Автор отсутствует в списке подписок"
-	return Response({"errors": error_message}, status=status.HTTP_400_BAD_REQUEST)
-
+        error_message = "Автор отсутствует в списке подписок"
+        return Response({"errors": error_message}, status=status.HTTP_400_BAD_REQUEST)
 
 class SubscriptionsView(ListAPIView):
     serializer_class = FollowSerializer
@@ -147,3 +142,4 @@ class SubscriptionsView(ListAPIView):
 
     def get_queryset(self):
         return self.request.user.follower.all()
+
