60ad5f6f5c985de7967c0bfeb76dfa6cad43d04f
ocronis <sorokidzes@yandex.ru>
2023-09-26 00:31:28 +0300

    second review

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404 
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
